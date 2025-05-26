69a5ceafecda075055294595b6269911ca29a6c8
mar-val5643 <maria_valliou@yahoo.com>
2025-05-26 14:24:09 +0300

    Update deploy.sh remove vpn

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,19 @@
 # Main script starts here
 
 # Check the number of arguments
-if [[ $# -ne 4 ]]; then
-    echo "Usage: $0 <site_id> <vpn_ip> <vpn_port> <vpn_password>"
-    exit 1
-fi
+# if [[ $# -ne 4 ]]; then
+#     echo "Usage: $0 <site_id> <vpn_ip> <vpn_port> <vpn_password>"
+#     exit 1
+# fi
 
-# Extract arguments
-site_id=$1
-vpn_ip=$2
-vpn_port=$3
-vpn_password=$4
+# # Extract arguments
+# site_id=$1
+# vpn_ip=$2
+# vpn_port=$3
+# vpn_password=$4
 
-# Create the ID file with the site_id
-echo "$1" > ID
+# # Create the ID file with the site_id
+# echo "$1" > ID
 
 # Change to the utils directory
 cd utils || { echo "Error - No utils folder"; exit 1; }
@@ -32,26 +32,26 @@ chmod +x console_command.sh
 
 # Check if OpenVPN connection is up
 #if sudo systemctl is-active --quiet openvpn@client.service >/dev/null; then
-if ip link show tun0 >/dev/null 2>&1; then
-    echo "VPN connection is already UP"
-    # Continue with the rest of the script
-else
-    # Establish VPN connection
-    ./establish_vpn_connection.sh $site_id $vpn_ip $vpn_port $vpn_password
-fi
+# if ip link show tun0 >/dev/null 2>&1; then
+#     echo "VPN connection is already UP"
+#     # Continue with the rest of the script
+# else
+#     # Establish VPN connection
+#     #./establish_vpn_connection.sh $site_id $vpn_ip $vpn_port $vpn_password
+# fi
 
 
 # Check the exit code of the script
-if [ $? -eq 0 ]; then
-    echo "VPN tunnel is up. Proceeding with deployment..."
-    # Continue with the rest of the deployment steps
-else
-    echo "Error: VPN connection is DOWN. Check your credentials."
-    echo "Deployment cannot proceed."
-    echo "Exiting..."
-    # Add error handling code or exit the script if necessary
-    exit 1
-fi
+# if [ $? -eq 0 ]; then
+#     echo "VPN tunnel is up. Proceeding with deployment..."
+#     # Continue with the rest of the deployment steps
+# else
+#     echo "Error: VPN connection is DOWN. Check your credentials."
+#     echo "Deployment cannot proceed."
+#     echo "Exiting..."
+#     # Add error handling code or exit the script if necessary
+#     exit 1
+# fi
 
 ./deploy_openhab.sh $site_id
 #./perform_operational_checks.sh  # skipped
