9912d486328f9e07cff7217f5866a5a79e063618
mar-val5643 <maria_valliou@yahoo.com>
2025-05-26 14:21:13 +0300

    Update deploy_openhab.sh  remove check if stick is there

--- utils/deploy_openhab.sh
+++ utils/deploy_openhab.sh
@@ -6,24 +6,24 @@ source ../config.env
 echo "Setting up openHAB..."
 
 # Check if Z-Wave controller is connected and /dev/ttyACM0 (or /dev/ttyUSB0 for Z-Stick 7) is available
-while true
-do
-  if ! lsusb | grep -q "$Z_STICK_MANUFACTURER"; then
-    echo "Z-Wave controller not detected. Please connect the $Z_STICK_NAME controller and press Enter to continue ..."
-  else
-    echo "Z-Wave controller connected."
-    if [ ! -e "$Z_STICK_PORT" ]; then
-      echo "$Z_STICK_PORT device not found. Please contact the UoP WSN Technical Consultant."
-      exit 1
-    fi
-    break
-  fi
+# while true
+# do
+#   if ! lsusb | grep -q "$Z_STICK_MANUFACTURER"; then
+#     echo "Z-Wave controller not detected. Please connect the $Z_STICK_NAME controller and press Enter to continue ..."
+#   else
+#     echo "Z-Wave controller connected."
+#     if [ ! -e "$Z_STICK_PORT" ]; then
+#       echo "$Z_STICK_PORT device not found. Please contact the UoP WSN Technical Consultant."
+#       exit 1
+#     fi
+#     break
+#   fi
 
-  # shellcheck disable=SC2162
-  read -p ""
-done
+#   # shellcheck disable=SC2162
+#   read -p ""
+# done
 
-echo "Continue with the rest of the deployment process ..."
+# echo "Continue with the rest of the deployment process ..."
 
 # Running docker compose
 sudo ../pythonvenv/bin/docker-compose --env-file ../config.env up --build -d
